671a4689b2c4025335f5ab05a1c04af3ef0dee3c
Gary Feltham <gary.feltham@citypay.com>
2017-05-19 12:16:34 +0100

    removed exec, found that errors could be propogated on validation routines

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ module.exports = function (cfg) {
 
     // set default placeholder functions if not supplied
     if (!cfg.err) {
-        cfg.err = () => {};
+        cfg.err = () => {
+        };
     }
     if (!cfg.failure) {
-        cfg.failure = () => {};
+        cfg.failure = () => {
+        };
     }
 
     // run some valiation first
@@ -28,40 +30,35 @@ module.exports = function (cfg) {
         return cfg.err("illegal argument. No success function supplied");
     }
 
-    return {
-        exec: function () {
+    // setup the captcha check and post off to Google for verification
+    // post data for sending to
+    let postData = `response=${encodeURIComponent(cfg.response)}&secret=${encodeURIComponent(cfg.secret)}`;
 
-            // setup the captcha check and post off to Google for verification
-            // post data for sending to
-            let postData = `response=${encodeURIComponent(cfg.response)}&secret=${encodeURIComponent(cfg.secret)}`;
-
-            // check the incoming captcha settings and if correct then continue
-            http.request({
-                hostname: 'www.google.com',
-                port: 443,
-                path: '/recaptcha/api/siteverify',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Content-Length': Buffer.byteLength(postData)
-                }
-            }, res => {
-                winston.info(`STATUS: ${res.statusCode}`);
-                res.setEncoding('utf8');
-                res.on('data', data => {
-                    winston.debug(`Google SiteVerify Response Data ${JSON.stringify(data)}`);
-                    if (data.success) {
-                        cfg.success();
-                    } else {
-                        cfg.failure(data);
-                    }
-                });
-            }).on('error', e => {
-                winston.info("Google SiteVerify error: " + e.message);
-                cfg.err(e.message);
-            }).write(postData);
+    // check the incoming captcha settings and if correct then continue
+    http.request({
+        hostname: 'www.google.com',
+        port: 443,
+        path: '/recaptcha/api/siteverify',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
         }
-    };
+    }, res => {
+        winston.info(`STATUS: ${res.statusCode}`);
+        res.setEncoding('utf8');
+        res.on('data', data => {
+            winston.debug(`Google SiteVerify Response Data ${JSON.stringify(data)}`);
+            if (data.success) {
+                cfg.success();
+            } else {
+                cfg.failure(data);
+            }
+        });
+    }).on('error', e => {
+        winston.info("Google SiteVerify error: " + e.message);
+        cfg.err(e.message);
+    }).write(postData);
 
 
 };
